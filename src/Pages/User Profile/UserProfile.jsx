0ebbcfcb2711740651940031c6b1cc40c913a37f
yousef <ucef123@gmail.com>
2025-06-17 20:04:22 +0300

    Allow userprofile to read data

--- src/Pages/User Profile/UserProfile.jsx
+++ src/Pages/User Profile/UserProfile.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import styles from "./UserProfile.module.css";
 import RecipeCard from "../../components/Common/RecipeCard/RecipeCard";
+import supabase from "../../config/supabaseClient";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 const UserProfile = () => {
+  const [error, setError] = useState(null);
+  const [recipes, setRecipes] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+        const { data, error } = await supabase
+          .from('recipe')
+          .select('*')
+
+          if (error) {
+            setError('Could not fetch recipes');
+            setRecipes(null);
+            console.log(error);
+          }
+          if (data) {
+            setRecipes(data);
+            setError(null);
+          }
+    }
+
+    fetchRecipes(); 
+  }, []);
+
+
+
+    console.log(supabase);
+
   return (
     <>
-      <h1>'s Recipe Box</h1>
+      <h1>'s Recipe Book</h1>
       <div className={styles.infoBox}>
         <div className={styles.userProfPic}>
           <div className={styles.userimg}></div>
@@ -28,18 +58,9 @@ const UserProfile = () => {
         <hr />
       </div>
       <div className={styles.userProfItems}>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-
+        {recipes && recipes.slice(0,6).map((recipe) => (
+            <RecipeCard key={recipe.recipe_id} recipe={recipe} />
+          ))}
       </div>
     </>
   );
