ba8b3d6df3486bd5b461a5c3baa0c1136c1106c8
ReemMK9 <reem.mo.khalaf@gmail.com>
2025-06-17 12:05:03 +0300

    home/popular categories responsive done

--- src/components/Home/PopularCategories.jsx
+++ src/components/Home/PopularCategories.jsx
@@ -4,16 +4,21 @@ import styles from "./PopularCategories.module.css";
 
 const PopularCategories = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.sectionTitle}>Popular Categories</h1>
-    <div className={styles.categoriesCards}>
-      <CircularCard />
-      <CircularCard />
-      <CircularCard />
-      <CircularCard />
-      <CircularCard />
-      <CircularCard />
-    </div>
+    <div className="container">
+      {" "}
+      {/* centers col-10 */}
+      <div className={`col-10 ml-0 ${styles.container}`}>
+        <h1 className={`ml-0 ${styles.sectionTitle}`}>Popular Categories</h1>
+        <div className="row gx-4 gy-5">
+          {" "}
+          {/* g-4 gives space between cards */}
+          {[...Array(6)].map((_, index) => (
+            <div key={index} className="col-6 col-md-3 col-xl-2 ml-0">
+              <CircularCard />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
