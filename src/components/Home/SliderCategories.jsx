475cd9a2b82ea2f01bd1eee4ec0d0920b40aea19
ReemMK9 <reem.mo.khalaf@gmail.com>
2025-06-17 03:31:11 +0300

    home/slider categories responsive done

--- src/components/Home/SliderCategories.jsx
+++ src/components/Home/SliderCategories.jsx
@@ -3,13 +3,17 @@ import styles from "../../components/Home/SliderCategories.module.css";
 import RecipeCard from "../Common/RecipeCard/RecipeCard";
 
 const SliderCategories = ({ recipes }) => {
-  console.log("recipes in SliderCategories:", recipes);
   return (
-    <div className={styles.container}>
+    <div className={`container ${styles.container}`}>
       <h1 className={styles.sliderTitle}>Sweet Tooth</h1>
-      <div className={styles.recipes}>
-        {recipes && recipes.slice(0,4).map((recipe) => (
-            <RecipeCard key={recipe.recipe_id} recipe={recipe} />
+
+      {/* Bootstrap row with gutter spacing */}
+      <div className="row g-4">
+        {recipes &&
+          recipes.slice(0, 4).map((recipe) => (
+            <div key={recipe.recipe_id} className="col-12 col-md-6 col-xl-3 d-flex">
+              <RecipeCard recipe={recipe} />
+            </div>
           ))}
       </div>
     </div>
