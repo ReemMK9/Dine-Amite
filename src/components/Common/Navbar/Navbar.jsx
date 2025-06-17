f5aba33bdfd19b48ae38ebe3d2a4f68840166ffe
ReemMK9 <reem.mo.khalaf@gmail.com>
2025-06-17 15:40:02 +0300

    Nav bar responsive and toggled mobile version

--- src/components/Common/Navbar/Navbar.jsx
+++ src/components/Common/Navbar/Navbar.jsx
@@ -4,39 +4,80 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navContainer}>
-      <Link to="/" className={styles.title}>
-        Dine'Amite
-      </Link>
-      <div className={styles.menu}>
-        <ul className={styles.menuItems}>
-          <li>
-            <Link to="/" >Home</Link>
-          </li>
-          <li>
-            <Link to="/recipes" >Recipes</Link>
-          </li>
-          {/* <li>
-            <Link to="">Blogs</Link>
-          </li>
-          <li>
-            <Link to="">Chefs</Link>
-          </li> */}
-        </ul>
-      </div>
-      <div className={styles.search}>
-        <input type="text" placeholder="Search..." />
-      </div>
-      <div className={styles.userIcon}>
-        <Link to="/userprofile">
-        <img
-          src="https://via.placeholder.com/40"
-          alt="User Icon"
-          className={styles.userIconImage}
-        />
-        </Link>
-      </div>
+    <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
+      <div className={`container-fluid ${styles.navContainer}`}>
+
+        {/* Small screen layout */}
+        <div className="d-lg-none w-100">
+          <div className="d-flex justify-content-between align-items-center">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarMenu"
+              aria-controls="navbarMenu"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className={styles.logoWrapper}>
+              <Link to="/" className={styles.title}>Dine'Amite</Link>
+            </div>
+          </div>
+
+          <div className={`collapse navbar-collapse ${styles.collapsibleMenu}`} id="navbarMenu">
+            <div className={`d-flex flex-column align-items-center py-3 ${styles.mobileMenuContent} ${styles.mobileMenuInner}`}>
+              <div className={styles.searchMobile}>
+                <input type="text" placeholder="Search..." />
+              </div>
+              <ul className="navbar-nav text-center mt-3">
+                <li className="nav-item">
+                  <Link to="/" className={styles.navLink}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/recipes" className={styles.navLink}>Recipes</Link>
+                </li>
+              </ul>
+              <div className={styles.userIconMobile}>
+                <Link to="/userprofile">
+                  <img
+                    src="https://via.placeholder.com/40"
+                    alt="User Icon"
+                    className={styles.userIconImage}
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Large screen layout */}
+        <div className="d-none d-lg-flex justify-content-between align-items-center w-100">
+          <Link to="/" className={styles.title}>Dine'Amite</Link>
+          <ul className={`navbar-nav ${styles.menuItems}`}>
+            <li className="nav-item">
+              <Link to="/" className={styles.navLink}>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/recipes" className={styles.navLink}>Recipes</Link>
+            </li>
+          </ul>
+          <div className={styles.search}>
+            <input type="text" placeholder="Search..." />
+          </div>
+          <div className={styles.userIcon}>
+            <Link to="/userprofile">
+              <img
+                src="https://via.placeholder.com/40"
+                alt="User Icon"
+                className={styles.userIconImage}
+              />
+            </Link>
+          </div>
+        </div>
+
       </div>
     </nav>
   );
