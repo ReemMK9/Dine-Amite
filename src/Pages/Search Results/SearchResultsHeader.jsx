73e4a3e2bb81fc5f6b8c1852689b00e306f93a16
yousef <ucef123@gmail.com>
2025-06-20 15:26:31 +0300

    Adjust styling and functionality of recipes and search results page

--- src/Pages/Search Results/SearchResultsHeader.jsx
+++ src/Pages/Search Results/SearchResultsHeader.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./SearchResultsHeader.module.css";
+import { useParams } from "react-router-dom"
 
 const SearchResultsHeader = () => {
+  const { query } = useParams();
   return (
     <div className={styles.container}>
       <h1>Search Results for:</h1>
-      <h3>Searched Words</h3>
-      <hr />
+      <h3>{ query }</h3>
+      {/* <hr /> */}
     </div>
   )
 }
